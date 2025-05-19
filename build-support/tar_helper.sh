8bbf4426bf4ea189773aef1e1a984db692fe6177
awewsomegamer <awewsomegamer@gmail.com>
2025-05-19 15:55:51 -0700

    Fixed up build system
    I have fixed up everything and it now works with the new tar_helper. I have also found a nicer way to share variables between Makefiles that use each other's sources.

--- build-support/tar_helper.sh
+++ build-support/tar_helper.sh
@@ -1,6 +1,7 @@
 # Download the given TAR if needed
 if [ ! -e $ARC_TAR ]; then
-        curl -o $ARC_TAR $ARC_MIRROR
+        echo "tar_helper: Downloading from $ARC_MIRROR into $ARC_TAR"
+        curl -o $ARC_TAR -L $ARC_MIRROR
 fi
 
 # If the TAR is present and it has not yet been extracted
@@ -9,8 +10,10 @@ if [ ! -d $ARC_SOURCE_DIR ] && [ -e $ARC_TAR ]; then
         tar -xf $ARC_TAR
         # Move it into *-src
         mv $ARC_NAME $ARC_SOURCE_DIR
+        echo "tar_helper: Extracted $ARC_TAR -> $ARC_SOURCE_DIR"
         # Apply necessary patch work
-        for f in $(find $(pwd) -type f -name "*.patch"); do
+        for f in $(find $(pwd) -maxdepth 1 -type f -name "*.patch"); do
+                echo "tar_helper: Applying patch $f to $ARC_SOURCE_DIR"
                 cd $ARC_NAME-src && patch -p1 < $f
         done
 fi
