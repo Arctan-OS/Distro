9ad9105f0a98ae758f14233963871e2d0511d6f2
awewsomegamer <awewsomegamer@gmail.com>
2025-10-21 17:35:18 -0500

    Change commands to use dynamic tool variables

--- build-support/tar_helper.sh
+++ build-support/tar_helper.sh
@@ -1,19 +1,19 @@
 # Download the given TAR if needed
 if [ ! -e $ARC_TAR ]; then
-        echo "tar_helper: Downloading from $ARC_MIRROR into $ARC_TAR"
-        curl -o $ARC_TAR -L $ARC_MIRROR
+        $ECHO "tar_helper: Downloading from $ARC_MIRROR into $ARC_TAR"
+        $CURL -o $ARC_TAR -L $ARC_MIRROR
 fi
 
 # If the TAR is present and it has not yet been extracted
 if [ ! -d $ARC_SOURCE_DIR ] && [ -e $ARC_TAR ]; then
         # Extract it
-        tar -xf $ARC_TAR
+        $TAR -xf $ARC_TAR
         # Move it into *-src
-        mv $ARC_NAME $ARC_SOURCE_DIR
-        echo "tar_helper: Extracted $ARC_TAR -> $ARC_SOURCE_DIR"
+        $MV $ARC_NAME $ARC_SOURCE_DIR
+        $ECHO "tar_helper: Extracted $ARC_TAR -> $ARC_SOURCE_DIR"
         # Apply necessary patch work
         for f in $(find $(pwd) -maxdepth 1 -type f -name "*.patch"); do
-                echo "tar_helper: Applying patch $f to $ARC_SOURCE_DIR"
-                cd $ARC_NAME-src && patch -p1 < $f
+                $ECHO "tar_helper: Applying patch $f to $ARC_SOURCE_DIR"
+                $CD $ARC_NAME-src && patch -p1 < $f
         done
 fi
