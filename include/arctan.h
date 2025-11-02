343fd5b27a90e8478cdc392b630f80766b426e04
awewsomegamer <awewsomegamer@gmail.com>
2025-11-02 17:11:03 -0600

    arctan.h: processor features
    The processor specific structures, enums, and values have been wrapped
    in an ifdef. Further, a new structure has been added to the kernel meta
    structure: processor features.

--- include/arctan.h
+++ include/arctan.h
@@ -4,34 +4,40 @@
 #include <stdint.h>
 #include <stddef.h>
 
-enum {
-	ARC_BOOTPROC_ARCTAN = 1,
-	ARC_BOOTPROC_MB2,
-	ARC_BOOTPROC_LBP,
-	ARC_BOOTPROC_MAX,
-};
+#ifdef ARC_TARGET_ARCH_X86_64
+        enum {
+  	        ARC_BOOTPROC_ARCTAN = 1,
+	        ARC_BOOTPROC_MB2,
+  	        ARC_BOOTPROC_LBP,
+	        ARC_BOOTPROC_MAX,
+        };
 
-enum {
-	ARC_MEMORY_BADRAM,
-	ARC_MEMORY_RESERVED,
-	ARC_MEMORY_ACPI_RECLAIMABLE,
-	ARC_MEMORY_NVS,
-	ARC_MEMORY_BOOTSTRAP,
-	ARC_MEMORY_BOOTSTRAP_ALLOC,
-	ARC_MEMORY_AVAILABLE,
-	ARC_MEMORY_MAX,
-};
+        enum {
+	        ARC_MEMORY_BADRAM,
+	        ARC_MEMORY_RESERVED,
+	        ARC_MEMORY_ACPI_RECLAIMABLE,
+	        ARC_MEMORY_NVS,
+	        ARC_MEMORY_BOOTSTRAP,
+	        ARC_MEMORY_BOOTSTRAP_ALLOC,
+	        ARC_MEMORY_AVAILABLE,
+	        ARC_MEMORY_MAX,
+        };
 
-enum {
-	ARC_PAGER_FLAG_NO_EXEC = 0,
-	ARC_PAGER_FLAG_1_GIB,
-	ARC_PAGER_FLAG_PCID,
-	ARC_PAGER_FLAG_PML5,
-	ARC_PAGER_FLAG_PKS,
-	ARC_PAGER_FLAG_MAX,
-};
+        enum {
+	        ARC_PAGER_FLAG_NX = 0,
+	        ARC_PAGER_FLAG_GIB,
+	        ARC_PAGER_FLAG_PCID,
+	        ARC_PAGER_FLAG_PML5,
+	        ARC_PAGER_FLAG_PKS,
+	        ARC_PAGER_FLAG_MAX,
+        };
 
-struct ARC_KernelMeta {
+        typedef struct ARC_ProcessorFeatures {
+	        uint64_t paging;
+        } ARC_ProcessorFeatures;
+#endif
+
+typedef struct ARC_KernelMeta{
 	/// Pointer to the base of the kernel module.
 	uint64_t kernel_elf;
 	struct {
@@ -52,18 +58,14 @@ struct ARC_KernelMeta {
 		uint64_t len;
 	} arc_mmap;
 	uint64_t rsdp;
-	uint64_t paging_features;
-	// Bit | Description (x86-64)
-	// 0   | NX bit is supported
-	// 1   | 1 GiB pages are supported
-}__attribute__((packed));
+	ARC_ProcessorFeatures features;
+}__attribute__((packed)) ARC_KernelMeta;
 
-struct ARC_BootMeta {
+typedef struct ARC_BootMeta {
 	struct {
 		uint64_t base;
 		uint64_t size;
 	} bsp_image;
-
 	uint64_t mem_size;
 	struct {
 		uint64_t base;
@@ -72,15 +74,15 @@ struct ARC_BootMeta {
 		int height;
 		int bpp;
 	} term;
-}__attribute__((packed));
+}__attribute__((packed)) ARC_BootMeta;
 
-struct ARC_MMap {
+typedef struct ARC_MMap {
 	uint64_t base;
 	uint64_t len;
 	int type;
-}__attribute__((packed));
+}__attribute__((packed)) ARC_MMap;
 
-struct ARC_PMMBiasConfigElement {
+typedef struct ARC_PMMBiasConfigElement {
 	size_t min_blocks;                    // The minimum number of 2^exp blocks that need
 					      // to be present in a region for it to initialized
 					      // as a pfreelist of 2^exp size blocks.
@@ -94,6 +96,6 @@ struct ARC_PMMBiasConfigElement {
 	} ratio;                              // The fraction that represents how much memory from a
 					      // region should be initialized into the pfreelist granted
 					      // at least min_blocks can be initialized.
-};
+} ARC_PMMBiasConfigElement;
 
 #endif
